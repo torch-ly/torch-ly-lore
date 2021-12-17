7b9145bca8ecbfda6415c3b3ad53a8588d8ce943
Ondolin <developer@ondolin.de>
2021-12-18 00:32:29 +0100

    made side cart rounded

--- src/components/View/SideCard.vue
+++ src/components/View/SideCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="clear-right bg-accent-light float-right p-4 w-1/6 min-w-max">
+  <div class="clear-right bg-accent-light float-right p-4 w-1/6 min-w-max rounded">
     <h2 class="mb-4 uppercase font-bold">Quick Information</h2>
 
     <slot />
