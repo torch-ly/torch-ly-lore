626fddea0994c4ac5330d13909fa432a935c6033
Ondolin <developer@ondolin.de>
2021-12-18 21:19:25 +0100

    finisched implementation for permissions

--- src/components/View/Permission/PermissionsSufficient.vue
+++ src/components/View/Permission/PermissionsSufficient.vue
@@ -4,6 +4,11 @@
   </div>
 </template>
 <script>
+
+/*
+ * TODO: This is a temporary implementation. It should be replaced with a proper one!
+ */
+
 export default {
   props: {
     permissions: {
@@ -11,22 +16,64 @@ export default {
       required: true
     }
   },
+  computed: {
+    campaign() {
+      return this.$store.state.campaign;
+    },
+    campaignUsers() {
+      return this.$store.state.campaign.users;
+    },
+    campaignPermissions() {
+      return this.campaign.permissions;
+    },
+  },
   data() {
     return {
-      permissionsTree: ['Taube', 'default'],
+      permissionsTree: [],
       canView: false
     }
   },
   mounted() {
+    this.generatePermissionTree()
     this.hasPermission()
+
+    setTimeout(() => {
+      this.generatePermissionTree()
+      this.hasPermission()
+    }, 100)
+
+    setTimeout(() => {
+      this.generatePermissionTree()
+      this.hasPermission()
+    }, 10000)
   },
   watch: {
     permissions() {
+      this.generatePermissionTree()
+      this.hasPermission()
+    },
+    userPermission() {
+      this.generatePermissionTree()
+      this.hasPermission()
+    },
+    campaignPermissions() {
+      this.generatePermissionTree()
+      this.hasPermission()
+    },
+    campaign() {
+      this.generatePermissionTree()
       this.hasPermission()
     }
   },
   methods: {
     hasPermission() {
+      if (this.permissionsTree.includes("gm")) {
+        this.canView = true;
+        this.$emit("canView");
+        this.$emit("canEdit");
+        this.$emit("canChangePermissions");
+        return;
+      }
       for (let perm of this.permissionsTree) {
         if (this.permissions[perm]) {
           if (this.permissions[perm] - 4 >= 0) {
@@ -41,7 +88,40 @@ export default {
           }
         }
       }
-    }
+    },
+    generatePermissionTree() {
+      let tree = [];
+      let subperm = this.userPermission();
+      while(subperm && subperm.name !== 'default' && subperm.name !== 'gm') {
+        tree.push(subperm.name)
+        let newSubperm = this.campaignPermissions.find((perm) => perm.name === subperm.root)
+        if (newSubperm) {
+          subperm = newSubperm
+        } else {
+          break
+        }
+      }
+      tree.push(subperm.name)
+      this.permissionsTree = tree;
+
+    },
+    userPermission() {
+      let perm =  this.campaignUsers.find((user) => {
+        return user.object.id === this.$store.state.user.uid
+      });
+
+      if (!perm) {
+        return {name: "", root: "default"};
+      } else {
+        perm = perm.permission;
+      }
+
+      return this.campaign.permissions.find((permission) => {
+        if (permission.name === perm) {
+          return permission;
+        }
+      });
+    },
   }
 }
 </script>
