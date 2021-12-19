b10684338035c980bc887d73eff861e45653f69e
Ondolin <developer@ondolin.de>
2021-12-19 23:04:10 +0100

    moved grid stuff to own component

--- src/components/View/CharacterGrid.vue
+++ src/components/View/CharacterGrid.vue
@@ -1,40 +1,23 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
-    <div v-for="char in $store.state.npcs" v-if="canView(char)">
-      <div class="w-64 h-48 p-4 border border-gray-200 rounded relative">
-        <span class="text-lg font-semibold">{{char.name}}</span>
-        <hr class="my-2">
-
-        <div>
-          <span>Status: </span>
-          <span :class="char.alive ? 'text-green-500' : 'text-red-500'">{{char.alive ? "Alive" : "Dead"}}</span>
-        </div>
-
-        <div v-if="getRaces(char)">
-          <span>Race: </span>
-          <span>{{ getRaces(char).content }}</span>
-        </div>
-
-        <div class="absolute right-2 bottom-2 flex">
-          <router-link v-if="canEdit(char)" :to="'/e/' + $store.state.campaign.id + '/char/' + char.id" class="button bg-green-400">Edit</router-link> <!-- Hide if this button can not be edited -->
-          <router-link :to="'/w/' + $store.state.campaign.id + '/char/' + char.id" class="ml-2 button">View</router-link>
-        </div>
-
-      </div>
+  <Grid :value="$store.state.npcs" :new-function="newCharacter" route="char" v-slot="val">
+    <div>
+      <span>Status: </span>
+      <span :class="val.alive ? 'text-green-500' : 'text-red-500'">{{ val.alive ? "Alive" : "Dead" }}</span>
     </div>
 
-    <PlusBox v-if="$store.state.userPermission.name !== 'default'" @click="newCharacter" class="w-64 h-48 p-4"/>
-
-  </div>
+    <div v-if="getRaces(val)">
+      <span>Race: </span>
+      <span>{{ getRaces(val).content }}</span>
+    </div>
+  </Grid>
 </template>
 <script>
 import {db} from "../../plugins/firebase";
 import router from "../../router";
-import PlusBox from "../PlusBox";
-import {hasFilePermission} from "../../plugins/permissions";
+import Grid from "./Grid";
 
 export default {
-  components: {PlusBox},
+  components: {Grid},
   methods: {
     async newCharacter() {
       let res = await db.collection('campaigns').doc(router.currentRoute.params.campaign).collection('npcs').add({
@@ -42,7 +25,7 @@ export default {
         alive: true,
         permissions: {
           default: 4, // Default group can view,
-          [this.$store.state.userPermission.name]: 7
+          [this.$store.state.userPermission.name]: 7 // you can view and edit
         }
       })
 
@@ -52,12 +35,7 @@ export default {
       if (!character || !character.quickinfo) return null;
       return character.quickinfo.find((info) => info.title === "Race");
     },
-    canView(character) {
-      return hasFilePermission(character.permissions).canView
-    },
-    canEdit(character) {
-      return hasFilePermission(character.permissions).canEdit
-    }
+
   }
 }
 </script>
---
+++ src/components/View/Grid.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+    <div v-for="val in value" v-if="canView(val)">
+      <div class="w-64 h-48 p-4 border border-gray-200 rounded relative">
+        <span class="text-lg font-semibold">{{ val.name }}</span>
+        <hr class="my-2">
+
+        <slot :val="val" />
+
+        <div class="absolute right-2 bottom-2 flex">
+          <router-link v-if="canEdit(val)" :to="'/e/' + $store.state.campaign.id + '/' + route + '/' + val.id"
+                       class="button bg-green-400">
+            Edit
+          </router-link>
+          <router-link :to="'/w/' + $store.state.campaign.id + '/' + route + '/' + val.id" class="ml-2 button">
+            View
+          </router-link>
+        </div>
+
+      </div>
+    </div>
+    <PlusBox v-if="$store.state.userPermission.name !== 'default'" @click="newFunction" class="w-64 h-48 p-4"/>
+  </div>
+</template>
+<script>
+import {hasFilePermission} from "../../plugins/permissions";
+import PlusBox from "../PlusBox";
+
+export default {
+  components: {
+    PlusBox
+  },
+  props: {
+    value: {
+      type: Array,
+      required: true
+    },
+    route: {
+      type: String,
+      required: true
+    },
+    newFunction: {
+      type: Function,
+      required: true
+    }
+  },
+  methods: {
+    canView(character) {
+      return hasFilePermission(character.permissions).canView
+    },
+    canEdit(character) {
+      return hasFilePermission(character.permissions).canEdit
+    }
+  }
+}
+</script>
