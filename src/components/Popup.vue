92516c33a7acd8b60b64e1a548dda399b3d17abc
Ondolin <developer@ondolin.de>
2021-12-19 19:23:38 +0100

    improved mobile popup design

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="" v-if="isOpen">
+  <div class="z-50" v-if="isOpen">
     <div class="fixed w-screen h-screen top-0 left-0 bg-gray-700 animateOpacity"/>
     <div class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center" @click="close">
-      <div class="w-1/3 bg-white rounded pt-4">
+      <div class="w-full mx-4 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white rounded pt-4">
 
-        <div class="flex mr-2">
+        <div class="flex flex-col lg:flex-row mr-2">
 
-          <div class="w-1/6 flex justify-center">
+          <div class="lg:w-1/6 flex justify-center">
             <div class="flex w-10 h-10 items-center justify-center bg-red-100 rounded-full"><font-awesome-icon class="text-red-500" icon="exclamation-triangle" /></div>
           </div>
 
-          <div class="w-5/6">
+          <div class="lg:w-5/6 m-3 lg:m-0 text-center lg:text-left">
             <h1 class="text-lg text-gray-900 leading-6 font-medium">{{ title }}</h1>
 
             <div class="text-gray-500 text-sm">
@@ -20,9 +20,9 @@
 
         </div>
 
-        <div class="text-black bg-gray-50 sm:flex sm:flex-row-reverse rounded-b mt-5 px-4 py-3 sm:px-6">
-          <button class="uppercase mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" @click="close">cancel</button>
-          <button class="uppercase w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm" @click="accept">yes</button>
+        <div class="text-black bg-gray-50 lg:flex lg:flex-row-reverse rounded-b mt-3 lg:mt-5 px-4 py-3 lg:px-6">
+          <button class="uppercase mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 lg:mt-0 lg:ml-3 lg:w-auto lg:text-sm" @click="close">cancel</button>
+          <button class="uppercase mt-3 lg:mt-0 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 lg:ml-3 lg:w-auto lg:text-sm" @click="accept">yes</button>
         </div>
 
       </div>
