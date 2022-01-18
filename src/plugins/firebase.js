20a58fa2384e76b7d761368687ca0d117e2cdc10
Ondolin <developer@ondolin.de>
2022-01-19 00:17:07 +0100

    added firebase

---
+++ src/plugins/firebase.js
@@ -0,0 +1,18 @@
+import 'firebase/firestore'
+import 'firebase/auth'
+//import { getAuth } from "firebase/auth"
+
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseApp = initializeApp({
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
+});
+
+export const db = getFirestore();
+//export const auth = getAuth();
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,19 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import { doc, onSnapshot } from "firebase/firestore";
+import {db} from "@/plugins/firebase";
+
+export default {
+  mounted() {
+
+    db.collection("campaigns").doc("Iqor").collection("npcs")
+        .onSnapshot((snap) => {
+          snap.docChanges().forEach((change) => {
+            console.log(change)
+          });
+        });
+  }
+}
+</script>
