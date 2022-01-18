b62215fc716fec50a243472e26db028192a79364
Ondolin <developer@ondolin.de>
2022-01-18 23:15:14 +0100

    added font awesome

--- src/main.js
+++ src/main.js
@@ -5,4 +5,13 @@ import router from './router'
 import store from './store'
 import './assets/tailwind.css'
 
-createApp(App).use(store).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+// library.add(faUserSecret)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .use(store)
+    .use(router)
+    .mount('#app')
