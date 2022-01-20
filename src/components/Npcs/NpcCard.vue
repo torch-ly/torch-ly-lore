fe319fc0e32e5a964d73df3934605b191d4c6c65
Ondolin <developer@ondolin.de>
2022-01-20 18:10:45 +0100

    removed debug statement

--- src/components/Npcs/NpcCard.vue
+++ src/components/Npcs/NpcCard.vue
@@ -1,6 +1,5 @@
 <template>
   <Card :title="value.name">
-    {{value}}
     <span :class="['px-2 py-1 text-green-800 text-xs font-medium rounded-full select-none', value.alive ? 'bg-green-100' : 'bg-red-100']">{{ value.alive ? 'alive' : 'dead' }}</span>
   </Card>
 </template>
